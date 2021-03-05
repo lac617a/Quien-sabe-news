a84984b9700c1578c7737fd169e703eef4fc519a
lac617a <botlacrita617@gmail.com>
2021-03-05 00:15:13 -0500

    arreglando la db and add aws sucees

--- tools/settings.py
+++ tools/settings.py
@@ -134,13 +134,13 @@ DATABASES={
     }
 }
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Password validation
