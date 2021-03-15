9eeda48be7d791e068996f82f4f6081587a75079
lac617a <botlacrita617@gmail.com>
2021-03-14 22:55:56 -0500

    arreglando los sitemap pero tranquilo que ya todo esta funcionando correctamente, ahora vamos a produccion

--- tools/urls.py
+++ tools/urls.py
@@ -14,23 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
-from django.contrib.sitemaps import GenericSitemap
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from news.models import NewNews
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
 
-info_dict = {'queryset':NewNews.objects.all(),}
+# add as many as sitemap you need as one key
 
+info_dict = {'queryset':NewNews.objects.all()}
 
 urlpatterns = [
     path('',include('news.urls')),
     path('ckeditor/',include('ckeditor_uploader.urls')),
     path('Category/',include('categories.urls')),
     path('hitcount/', include(('hitcount.urls', 'hitcount'), namespace='hitcount')),
-    path('sitemap.xml',sitemap,{'sitemaps':{'blog':GenericSitemap(info_dict,priority=0.6)}},name='django.contrib.sitemaps.views.sitemap'),
-    path('18-qsn-admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': {'blog':GenericSitemap(info_dict,priority=0.6,protocol='https')}},name='django.contrib.sitemaps.views.sitemap'),
+    path('18-qsnoticia-forever/', admin.site.urls),
 ]
 
 if settings.DEBUG: # DEV ONLY
