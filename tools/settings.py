800fe3dadf323b87c6a36ce411add4393b8cd5af
lac617a <botlacrita617@gmail.com>
2021-03-22 15:46:48 -0500

    add bitly to .env

--- tools/settings.py
+++ tools/settings.py
@@ -183,9 +183,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static' ),)
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = (os.path.join(BASE_DIR,'static' ),)
 
 from .AWS import *
 
